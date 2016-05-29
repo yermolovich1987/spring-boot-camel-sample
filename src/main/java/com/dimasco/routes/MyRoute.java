ebd6790bd814d9a288b1b0bf166856a3d07b0d4f
dimas <yermolovich1987@gmail.com>
2016-05-29 18:49:25 +0300

    Basic application. Template for future researches.

---
+++ src/main/java/com/dimasco/routes/MyRoute.java
@@ -0,0 +1,13 @@
+package com.dimasco.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo").to("log:bar");
+    }
+}
